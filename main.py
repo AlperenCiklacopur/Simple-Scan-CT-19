77b478e5ff9856ce44aaf59948430e71db108830
AlperenCiklacopur <76427858+AlperenCiklacopur@users.noreply.github.com>
2021-05-27 12:26:57 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,112 @@
+import os
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+from tqdm.notebook import tqdm
+import pandas as pd
+from keras.utils import plot_model
+from keras import models
+from keras import layers
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score, roc_curve, auc, precision_score, accuracy_score, confusion_matrix
+
+def read_image(filepath):
+    return cv2.imread(os.path.join(data_dir, filepath))
+
+def resize_image(image, image_size):
+    return cv2.resize(image.copy(), image_size, interpolation = cv2.INTER_AREA)
+
+disease_types=['COVID', 'non-COVID']
+data_dir = 'C:/Users/Alperen/PycharmProjects/pythonProject2/kaggle/input'
+train_dir = os.path.join(data_dir)
+
+train_data = []
+for defects_id, sp in enumerate(disease_types):
+    for file in os.listdir(os.path.join(train_dir, sp)):
+        train_data.append(['{}/{}'.format(sp, file), defects_id, sp])
+train = pd.DataFrame(train_data, columns=['File', 'DiseaseID', 'Disease Type'])
+
+
+IMAGE_SIZE = 64
+X = np.zeros((train.shape[0], IMAGE_SIZE, IMAGE_SIZE, 3))
+for i, file in tqdm(enumerate(train['File'].values), total = len(train)):
+    image = read_image(file)
+    if image is not None:
+        X[i] = resize_image(image, (IMAGE_SIZE, IMAGE_SIZE))
+X /= 255.
+y = train['DiseaseID'].values
+print(X.shape)
+print(y.shape)
+
+x_train, x_test, y_train, y_test = train_test_split(X, y, test_size=0.2, stratify=y, random_state = 1)
+x_train[:2]
+y[:]
+
+
+np.random.seed(3)
+
+model = models.Sequential()
+model.add(layers.Conv2D(64, (3, 3), activation='relu', input_shape=(64, 64, 3)))
+model.add(layers.MaxPool2D((2, 2)))
+model.add(layers.Conv2D(256, (3, 3), activation='relu'))
+model.add(layers.MaxPool2D((2, 2)))
+model.add(layers.Dropout(0.5))
+model.add(layers.Conv2D(256, (3, 3), activation='relu'))
+model.add(layers.MaxPool2D((2, 2)))
+model.add(layers.Conv2D(256, (3, 3), activation='relu'))
+
+model.add(layers.Dropout(0.5))
+model.add(layers.Conv2D(528, (3, 3), activation='relu'))
+model.add(layers.MaxPool2D((2, 2)))
+
+
+model.add(layers.Flatten())
+model.add(layers.Dense(128, activation='relu'))
+model.add(layers.Dense(64, activation='relu'))
+model.add(layers.Dense(32, activation='relu'))
+model.add(layers.Dense(1, activation='sigmoid'))
+
+
+plot_model(model,
+           show_shapes = True,
+           show_layer_names = True,
+           rankdir = 'TB',
+           expand_nested = False,
+           dpi = 60)
+
+model.summary()
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['accuracy'])
+history = model.fit(x_train, y_train,
+                    batch_size=64, epochs=30,
+                    verbose=1,validation_split=0.1)
+
+y_pred = model.predict_classes(x_test)
+
+fpr = dict()
+tpr = dict()
+roc_auc = dict()
+for i in range(2):
+    fpr[i], tpr[i], _ = roc_curve(y_test, y_pred)
+    roc_auc[i] = auc(fpr[i], tpr[i])
+
+plt.figure()
+plt.plot(fpr[1], tpr[1])
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic')
+plt.show()
+
+tn, fp, fn, tp = confusion_matrix(y_test, y_pred).ravel()
+
+
+sp = tn/(tn+fp)
+sn = tp/(tp+fn)
+
+print('f1 score =  %.3f'%f1_score(y_test, y_pred))
+print('Precision =  %.3f'%precision_score(y_test, y_pred))
+print('Test accuracy =  %.3f'%accuracy_score(y_test, y_pred))
+print('Specificity =  %.3f'%sp)
+print('Sensitivity =  %.3f'%sn)
